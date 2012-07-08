6af00e094a189a38398c8adfb4009e59c7c901d9
Sebastian Schuberth <sschuberth@gmail.com>
2012-07-08 22:22:26 +0200

    Add a script to show the changes / conflict resolution of a merge commit
    For details see:
    
        http://stackoverflow.com/questions/10998728/view-already-committed-git-merge-in-external-3-way-diff-tool/

---
+++ git-show-merge-resolution.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "Rationale : Show the conflict resolution of a given merge commit in the configured merge tool."
+    echo "Usage : $(basename $0) <merge commit>"
+    exit -1
+fi
+
+# Test e.g. with https://github.com/git/git/commit/8cde60210dd01f23d89d9eb8b6f08fb9ef3a11b8
+our=$1^1
+their=$1^2
+base=$(git merge-base $our $their)
+
+conflicting_files=$(git merge-tree $base $our $their | grep -A 3 "changed in both" | grep "base" | grep -Po "[^\s]+$")
+for f in $conflicting_files; do
+    diffuse -r $our -r $base -r $their $f
+done
