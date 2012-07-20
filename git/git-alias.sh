b3ff0f516e92b2db8caa79ce1be5795f968fa05b
Sebastian Schuberth <sschuberth@gmail.com>
2012-07-20 12:35:22 +0200

    Add a script with useful git aliases

---
+++ git/git-alias.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Git Root
+alias gr='cd ./$(git rev-parse --show-cdup 2> /dev/null)'
+
+# Git Get Upstream
+alias ggu='git br -vv | sed -nr "s/^\*.*\[(.*)\].*$/\1/p"'
+
+# Git Fetch Prune
+alias gfp='git fetch && git remote prune origin'
+
+# Git Pull Prune
+alias gpp='git pull --rebase && git remote prune origin'
+
+# Git Compare Differences
+function git_cmp_diffs() {
+    if [ $(git show $1 | git patch-id | cut -c 40) != $(git show $2 | git patch-id | cut -c 40) ]; then
+        echo "Diffs are NOT equal."
+    else
+        echo "Diffs ARE equal."
+    fi
+}
+
+alias gcd='git_cmp_diffs'
+
+# Git Is Merged
+function git_is_merged() {
+    revlist=$(git rev-list -1 $1 --not $2)
+    if [ $? -eq 0 ]; then
+        if [ "$revlist" = "" ]; then
+            echo "$(basename $1) IS merged into $(basename $2)."
+        else
+            echo "$(basename $1) is NOT merged into $(basename $2)."
+        fi
+    fi
+}
+
+alias gim='git_is_merged'
