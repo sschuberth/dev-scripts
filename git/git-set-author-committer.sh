806e95dabc36531879bf21a7b6fbe21d7bfe4364
Sebastian Schuberth <sschuberth@gmail.com>
2012-12-08 23:27:40 +0100

    Add an option to specify author and committer independently

--- git/git-set-author-committer.sh
+++ git/git-set-author-committer.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-if [ $# -ne 3 ]; then
+if [[ $# -ne 3 && $# -ne 5 ]]; then
     echo "Rationale : Set the author and committer name and email for a number of recent commits."
-    echo "Usage     : $(basename $0) <number of commits> <name> <email>"
+    echo "Usage     : $(basename $0) <number of commits> <name> <email> [<name> <email>]"
     exit -1
 fi
 
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='$2'; GIT_AUTHOR_EMAIL='$3'; GIT_COMMITTER_NAME='$2'; GIT_COMMITTER_EMAIL='$3';" HEAD~$1..HEAD
+an=$2
+ae=$3
+if [ $# -eq 3 ]; then
+    cn=$2
+    ce=$3
+else
+    cn=$4
+    ce=$5
+fi
+
+git filter-branch -f --env-filter "GIT_AUTHOR_NAME='$an'; GIT_AUTHOR_EMAIL='$ae'; GIT_COMMITTER_NAME='$cn'; GIT_COMMITTER_EMAIL='$ce';" HEAD~$1..HEAD
