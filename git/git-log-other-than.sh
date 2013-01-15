b073a727fd7d76f475f97e2b031d67442031f7e0
Sebastian Schuberth <sschuberth@gmail.com>
2013-01-15 12:15:39 +0100

    Add a script to list all commits that touch files other than those matching a pattern

---
+++ git/git-log-other-than.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+    echo "Rationale : Lists all commits that touch any files other than those matching a regex pattern."
+    echo "Usage     : $(basename $0) <branch> <pattern>"
+    exit -1
+fi
+
+for commit in $(git rev-list $1); do
+    names=$(git show --pretty="format:" --name-only $commit | tail -n +2)
+    if echo "$names" | grep -qv $2; then
+        echo "$commit touches other files"
+    fi
+done
