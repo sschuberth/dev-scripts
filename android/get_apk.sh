f9d0337df5b1225709f175a52a630c023c7c6cd5
Sebastian Schuberth <sschuberth@gmail.com>
2013-07-17 15:34:21 +0200

    android: Make get_apk.sh work under MinGW / MSYS

--- android/get_apk.sh
+++ android/get_apk.sh
@@ -11,7 +11,14 @@ packages=$(adb shell pm list packages | cut -d : -f 2)
 if [ $# -eq 0 ]; then
     echo "$packages"
 else
+    dir=/data/app
+    case $(uname -s) in
+    MINGW*)
+        # Use a double-slash in the beginning to prevent MSYS path mangling.
+        dir=/$dir
+        ;;
+    esac
     for p in $(echo "$packages" | grep $1); do
-        adb pull /data/app/$p-1.apk
+        adb pull $dir/$p-1.apk
     done
 fi
