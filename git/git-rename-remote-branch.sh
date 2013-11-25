1f1d7955e613713dc1815bc6b7e2896a6cd75197
Sebastian Schuberth <sschuberth@gmail.com>
2013-11-25 10:40:43 +0100

    git: Add a script to rename a branch on the server without checking it out

---
+++ git/git-rename-remote-branch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]; then
+    echo "Rationale : Rename a branch on the server without checking it out."
+    echo "Usage     : $(basename $0) <remote> <old name> <new name>"
+    echo "Example   : $(basename $0) origin master release"
+    exit -1
+fi
+
+git push $1 $1/$2:refs/heads/$3 && git push $1 :$2
