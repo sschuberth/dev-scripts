0987be2a41834b73d8dfb5dddcfb16ffdf976406
Sebastian Schuberth <sschuberth@gmail.com>
2013-11-26 15:25:35 +0100

    android: Add a script to pull files from the device by wildcard
    See http://stackoverflow.com/questions/11074671/adb-pull-multiple-files.

---
+++ android/pull_all.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "Rationale : Pull files from the device by wildcard."
+    echo "Usage : $(basename $0) <path>"
+    exit -1
+fi
+
+adb shell ls $1 | tr -s "\n\r" "\0" | xargs -0 -n1 adb pull
