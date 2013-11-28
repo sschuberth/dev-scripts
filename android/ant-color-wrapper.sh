ee55f78d26440c03da500a848d118979cb7bc73f
Sebastian Schuberth <sschuberth@gmail.com>
2013-11-28 12:17:11 +0100

    ant-color-wrapper: Also pipe STDERR through Perl to get errors colored

--- android/ant-color-wrapper.sh
+++ android/ant-color-wrapper.sh
@@ -2,4 +2,4 @@
 
 # Make sure to execute the command, not the alias shadowing it, see:
 # http://unix.stackexchange.com/questions/39291/run-a-command-that-is-shadowed-by-an-alias
-"ant" -logger org.apache.tools.ant.listener.AnsiColorLogger "$@" | perl -pe 's/(?<=\e\[)2;//g'
+"ant" -logger org.apache.tools.ant.listener.AnsiColorLogger "$@" 2>&1 | perl -pe 's/(?<=\e\[)2;//g'
