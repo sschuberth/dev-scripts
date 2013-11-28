8fc97cade3133e4bc323d1ebced004af4067daa5
Sebastian Schuberth <sschuberth@gmail.com>
2013-11-28 12:22:49 +0100

    ant-color-wrapper: Add some more comments and links

--- android/ant-color-wrapper.sh
+++ android/ant-color-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# Make sure to execute the command, not the alias shadowing it, see:
-# http://unix.stackexchange.com/questions/39291/run-a-command-that-is-shadowed-by-an-alias
+# The general solution is taken from http://stackoverflow.com/a/8129450/1127485.
+# Quote the "ant" command to make sure not any alias of the same name is executed, see http://unix.stackexchange.com/a/39296/53328.
 "ant" -logger org.apache.tools.ant.listener.AnsiColorLogger "$@" 2>&1 | perl -pe 's/(?<=\e\[)2;//g'
