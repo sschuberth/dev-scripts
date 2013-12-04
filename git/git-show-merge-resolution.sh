890645fd45476b86706f8920938e8aa209d20111
Sebastian Schuberth <sschuberth@gmail.com>
2013-12-04 16:53:38 +0100

    git-show-merge-resolution: Really use the configured merge tool
    Rework the script to really use the configured merge tool instead of
    diffuse and to properly show conflicting files only, or all files that
    were concurrently changed in both parents.

--- git/git-show-merge-resolution.sh
+++ git/git-show-merge-resolution.sh
@@ -1,17 +1,80 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-    echo "Rationale : Show the conflict resolution of a given merge commit in the configured merge tool."
-    echo "Usage     : $(basename $0) <merge commit>"
+if [ $# -lt 1 -o $# -gt 2 ]; then
+    echo "Rationale : Show the resolution of a given merge commit in the configured merge tool."
+    echo "Usage     : $(basename $0) [-c] <merge commit>"
     exit -1
 fi
 
-# Test e.g. with https://github.com/git/git/commit/8cde60210dd01f23d89d9eb8b6f08fb9ef3a11b8
-our=$1^1
-their=$1^2
-base=$(git merge-base $our $their)
+die() {
+    echo >&2 "$@"
+    exit 1
+}
 
-conflicting_files=$(git merge-tree $base $our $their | grep -A 3 "changed in both" | grep "base" | cut -d " " -f 8)
-for f in $conflicting_files; do
-    diffuse -r $our -r $base -r $their $f
+if [ "$1" == "-c" ]; then
+    conflicts_only=true
+    shift
+else
+    conflicts_only=false
+fi
+
+ours=$1^1
+theirs=$1^2
+base=$(git merge-base $ours $theirs)
+
+if $conflicts_only; then
+    files=$(git show $1 | grep "diff --cc" | cut -d " " -f 3)
+else
+    files=$(git merge-tree $base $ours $theirs | grep -A 3 "changed in both" | grep "base" | cut -d " " -f 8)
+fi
+
+if [ -n "$files" ]; then
+    count=$(echo "$files" | wc -l)
+    count=$(echo $count)
+else
+    count=0
+fi
+
+if $conflicts_only; then
+    if [ $count == 0 ]; then
+        echo "There were no conflicting files in that merge commit."
+    elif [ $count == 1 ]; then
+        echo "There was one conflicting file in that merge commit:"
+    else
+        echo "There were $count conflicting files in that merge commit:"
+    fi
+else
+    if [ $count == 0 ]; then
+        echo "There were no files concurrently changed in both parents."
+    elif [ $count == 1 ]; then
+        echo "There was one file concurrently changed in both parents:"
+    else
+        echo "There were $count files concurrently changed in both parents:"
+    fi
+fi
+
+for f in $files; do
+    echo $f
+done
+
+TOOL_MODE=merge
+: ${MERGE_TOOL_LIB=$(git --exec-path)/git-mergetool--lib}
+. "$MERGE_TOOL_LIB"
+
+merge_tool=$(get_merge_tool "$merge_tool") || die "Error: No merge tool configured."
+
+for f in $files; do
+    LOCAL="/tmp/$f.LOCAL"
+    BASE="/tmp/$f.BASE"
+    REMOTE="/tmp/$f.REMOTE"
+    MERGED="/tmp/$1.MERGED"
+
+    BACKUP="/tmp/$1.BACKUP"
+
+    mkdir -p $(dirname $LOCAL) && git show $ours:"$f" > $LOCAL
+    mkdir -p $(dirname $BASE) && git show $base:"$f" > $BASE
+    mkdir -p $(dirname $REMOTE) && git show $theirs:"$f" > $REMOTE
+    mkdir -p $(dirname $MERGED) && git show $1:"$f" > $MERGED
+
+    run_merge_tool "$merge_tool" "true"
 done
