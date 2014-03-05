b4cdf7aec6f3ad443ebd52b2b84b9560a06e5d3f
Sebastian Schuberth <sschuberth@gmail.com>
2014-03-05 15:52:10 +0100

    gerrit-push-for: Determine potential reviewers using git-contacts
    Get git-contacts, which requires Perl, at:
    
    https://github.com/git/git/blob/master/contrib/contacts/git-contacts

--- gerrit/gerrit-push-for.sh
+++ gerrit/gerrit-push-for.sh
@@ -18,11 +18,39 @@ else
     remote=$(echo "$remotes" | head -1)
 fi
 
+# Determine email addresses of potential reviewers (except oneself).
+git contacts 2> /dev/null
+if [ $? -eq 9 ]; then
+    user=$(git config user.name)
+    reviewers=$(git contacts $remote/$target..HEAD | grep -iv "$user" | cut -d "<" -f 2 | cut -d ">" -f 1)
+    for email in $reviewers; do
+        if [ -z "$r" ]; then
+            r="r=$email"
+        else
+            r="$r,$email"
+        fi
+    done
+fi
+
+# Determine the reviewer count, stripping leading whitespace.
+count=$(echo "$reviewers" | wc -l)
+count=$(echo $count)
+
+# Specify a topic if we are on a branch different from the target.
 topic=$(git rev-parse --abbrev-ref HEAD)
 if [ $topic != $target ]; then
-    echo "Pushing topic \"$topic\" to remote \"$remote\"..."
-    git push $remote HEAD:refs/for/$target%topic=$topic
+    echo "Pushing topic \"$topic\" to remote \"$remote\" with $count reviewer(s) set."
+    options="%topic=$topic"
 else
-    echo "Pushing to remote \"$remote\"..."
-    git push $remote HEAD:refs/for/$target
+    echo "Pushing to remote \"$remote\" with $count reviewer(s) set."
 fi
+
+if [ -n "$r" ]; then
+    if [ -z "$options" ]; then
+        options="%$r"
+    else
+        options="$options,$r"
+    fi
+fi
+
+git push $remote HEAD:refs/for/$target$options
