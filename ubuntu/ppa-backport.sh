813f937124ef10cee08ba7ef72730bb56cbf0a21
Sebastian Schuberth <sschuberth@gmail.com>
2016-04-18 13:38:12 +0200

    ubuntu: Add a basic script to backport packages

---
+++ ubuntu/ppa-backport.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $(basename $0) <package> <release> <ppa>"
+    exit 1
+fi
+
+# Get the latest available version of the package.
+package=$(apt-cache search $1 | tail -1 | cut -d" " -f1)
+
+# Find out the source package it belogs to.
+message=$(apt-get source -s $package | grep "as source package")
+echo $message
+
+source=$(echo $message | cut -d"'" -f2)
+
+# Backport the package. For details see
+# https://opensourcehacker.com/2013/03/20/how-to-backport-packages-on-ubuntu-linux/
+backportpackage -d $2 -u ppa:$3 -y $source
