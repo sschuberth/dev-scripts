36568d77fe63414455fe803856d05a8dd5494ce1
Sebastian Schuberth <sschuberth@gmail.com>
2016-05-18 13:04:44 +0200

    git-hash-blob: Fix hashing text files with DOS line endings

--- git/git-hash-blob.sh
+++ git/git-hash-blob.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-(stat --printf="blob %s\0" "$1"; cat "$1") | sha1sum -b | cut -d" " -f1
+# This script calculates the same as "git hash-object $1".
+
+infile=$1
+outfile=$(mktemp)
+
+if !(dos2unix -n "$infile" "$outfile" 2>&1 | grep -q "Skipping binary file"); then
+    infile=$outfile
+fi
+
+(stat --printf="blob %s\0" "$infile"; cat "$infile") | sha1sum -b | cut -d" " -f1
+
+rm -f "$outfile"
