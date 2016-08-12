85644663661ef01546f9f11a75d631d8f28f0123
Sebastian Schuberth <sschuberth@gmail.com>
2016-08-12 10:00:40 +0200

    ppa-backport: Add basic support for fetch the DSC from an URL

--- ubuntu/ppa-backport.sh
+++ ubuntu/ppa-backport.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $# -ne 3 ]; then
     echo "Usage   : $(basename $0) <package> <release> <ppa>"
@@ -6,15 +6,19 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
-# Get the name of the latest available package (in case of multiple matching package names).
-package=$(apt-cache search "^$1(-[0-9\.]+)?$" | tail -1 | cut -d" " -f1)
-echo "Using package name '$package'"
+if [[ "$1" != *.dsc ]]; then
+    # Get the name of the latest available package (in case of multiple matching package names).
+    package=$(apt-cache search "^$1(-[0-9\.]+)?$" | tail -1 | cut -d" " -f1)
+    echo "Using package name '$package'"
 
-# Find out the source package it belogs to.
-message=$(apt-get source -s $package)
-echo "$message" | grep "^Picking.*as source package"
-source=$(echo "$message" | tail -1 | cut -d" " -f3)
-echo "Using source package name '$source'"
+    # Find out the source package it belogs to.
+    message=$(apt-get source -s $package)
+    echo "$message" | grep "^Picking.*as source package"
+    source=$(echo "$message" | tail -1 | cut -d" " -f3)
+    echo "Using source package name '$source'"
+else
+    source=$1
+fi
 
 # Backport the package. For details see
 # https://opensourcehacker.com/2013/03/20/how-to-backport-packages-on-ubuntu-linux/
