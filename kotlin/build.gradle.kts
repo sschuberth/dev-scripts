41bd0c6e7f039d74a6aac512012ddbe5ea12cabd
Sebastian Schuberth <sschuberth@gmail.com>
2024-03-09 17:06:09 +0100

    Add a Kotlin script to convert a JSON schema to data classes

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,2 @@
+// This is a dummy file to make Fleet recognize `.main.kts` scripts in this folder, see
+// https://youtrack.jetbrains.com/issue/FL-22261.
---
+++ kotlin/json-kotlin-schema-codegen.main.kts
@@ -0,0 +1,50 @@
+#!/usr/bin/env kotlin
+
+@file:DependsOn("com.github.ajalt.clikt:clikt-jvm:4.2.2")
+@file:DependsOn("net.pwall.json:json-kotlin-schema-codegen:0.107")
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.output.MordantHelpFormatter
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.mordant.rendering.Theme
+
+import java.io.File
+import java.net.URI
+
+import net.pwall.json.schema.codegen.CodeGenerator
+
+object : CliktCommand(name = __FILE__.name) {
+    val schema by argument()
+
+    val packageName by option()
+        .default("com.example")
+
+    val outputDirectory by option()
+        .file(mustExist = false, canBeFile = false, canBeDir = true, mustBeWritable = true, mustBeReadable = false, canBeSymlink = true)
+        .default(File("."))
+
+    init {
+        context {
+            helpFormatter = { MordantHelpFormatter(context = it, "*", showDefaultValues = true) }
+        }
+    }
+
+    override fun run() {
+        val schemaUri = URI.create(schema).takeIf { it.isAbsolute } ?: File(schema).toURI()
+
+        val codeGenerator = CodeGenerator().apply {
+            basePackageName = packageName
+            baseDirectoryName = outputDirectory.path
+        }
+
+        runCatching {
+            codeGenerator.generate(schemaUri)
+        }.onFailure {
+            echo(Theme.Default.danger(it.message.orEmpty()))
+        }
+    }
+}.main(args)
