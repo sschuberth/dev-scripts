f38e72a902f5f8182694137b81eaea5e8e88b104
Sebastian Schuberth <sschuberth@gmail.com>
2024-09-04 19:27:11 +0200

    Remove unused stuff

--- android/ant_color_wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# The general solution is taken from http://stackoverflow.com/a/8129450/1127485.
-# Quote the "ant" command to make sure not any alias of the same name is executed, see http://unix.stackexchange.com/a/39296/53328.
-"ant" -logger org.apache.tools.ant.listener.AnsiColorLogger "$@" 2>&1 | perl -pe 's/(?<=\e\[)2;//g'
