67691002e14b1a4ee6d1fd33e454b790550cfd24
Sebastian Schuberth <sschuberth@gmail.com>
2024-11-20 16:42:21 +0100

    deps(kotlin): Upgrade clikt and codegen for the schema script

--- kotlin/json-kotlin-schema-codegen.main.kts
+++ kotlin/json-kotlin-schema-codegen.main.kts
@@ -1,10 +1,11 @@
 #!/usr/bin/env kotlin
 
-@file:DependsOn("com.github.ajalt.clikt:clikt-jvm:4.2.2")
-@file:DependsOn("net.pwall.json:json-kotlin-schema-codegen:0.107")
+@file:DependsOn("com.github.ajalt.clikt:clikt-jvm:5.0.1")
+@file:DependsOn("net.pwall.json:json-kotlin-schema-codegen:0.113")
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.output.MordantHelpFormatter
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.default
