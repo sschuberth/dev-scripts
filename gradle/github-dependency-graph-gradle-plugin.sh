6564b2e253edb2e059d020c2d7e7a3d2e6ffe4d9
Sebastian Schuberth <sschuberth@gmail.com>
2025-12-09 08:03:17 +0100

    Add some basic usage instructions to a Gradle script

--- gradle/github-dependency-graph-gradle-plugin.sh
+++ gradle/github-dependency-graph-gradle-plugin.sh
@@ -2,6 +2,11 @@
 
 SCRIPT_DIR="$(cd "$(dirname $0)" && pwd)"
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $(basename $0) <project dir>"
+    exit 1
+fi
+
 pushd $1 > /dev/null
 
 ./gradlew -I $SCRIPT_DIR/github-dependency-graph-gradle-plugin.init.gradle ForceDependencyResolutionPlugin_resolveAllDependencies
